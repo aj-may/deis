afa9bb01cbfe1d0d6115796f8925a41b17de3978
v-reyder <v.reyder@2gis.ru>
2015-08-25 15:15:32 +0600

    fix(logger): Rework logger to use sync.Mutex instead of channels

--- logger/handler/ringbuffer.go
+++ logger/handler/ringbuffer.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"regexp"
 	"strconv"
+	"sync"
 
 	"github.com/deis/deis/logger/drain"
 	"github.com/deis/deis/logger/syslog"
@@ -14,24 +15,16 @@ import (
 
 var logStorage = make(map[string]*ring.Ring)
 var ringBufferSize int
-var inChannel chan *protocol
-var outChannel chan *protocol
 
 var regexpForPost = regexp.MustCompile(`^/([-a-z0-9]+)/.*`)
 var regexpForGet = regexp.MustCompile(`^/([-a-z0-9]+)/([0-9]+)/.*`)
 
-const TYPE_PUT = 0
-const TYPE_REQUEST = 1
-
-type protocol struct {
-    Type int
-    LinesNumber int
-    ProjectName string
-    Payload string
-}
+var mu sync.Mutex
 
 // Add log message to main map with ring byffers by project name
 func addToStorage(name string, message string) {
+	mu.Lock()
+	defer mu.Unlock()
 	currentRing, ok := logStorage[name]
 	if !ok {
 		r := ring.New(ringBufferSize)
@@ -46,9 +39,11 @@ func addToStorage(name string, message string) {
 
 // Get specific amount of log lines for application name from main map with ring buffers
 func getFromStorage(name string, lines int) string {
+	mu.Lock()
+	defer mu.Unlock()
 	currentRing, ok := logStorage[name]
 	if !ok {
-        return fmt.Sprintf("Could not find logs for project '%s'", name)
+		return fmt.Sprintf("Could not find logs for project '%s'", name)
 	}
 	var data string
 	getLine := func(line interface{}) {
@@ -56,7 +51,11 @@ func getFromStorage(name string, lines int) string {
 			return
 		}
 		lines -= 1
-		data += fmt.Sprintln(line)
+		if lines == 0 {
+			data += fmt.Sprint(line)
+		} else {
+			data += fmt.Sprintln(line)
+		}
 	}
 
 	currentRing.Next().Do(getLine)
@@ -64,8 +63,6 @@ func getFromStorage(name string, lines int) string {
 }
 
 func RingBufferHandler(bufferSize int, webServicePort int) *Handler {
-    inChannel = make(chan *protocol)
-    outChannel = make(chan *protocol)
 	ringBufferSize = bufferSize
 	h := Handler{
 		BaseHandler: syslog.NewBaseHandler(5, filter, false),
@@ -78,10 +75,9 @@ func RingBufferHandler(bufferSize int, webServicePort int) *Handler {
 
 // Main loop for this handler, each log line will be sended to drain (if DrainURI specified) and copied to log storage
 func (h *Handler) ringBufferLoop() {
-    go accessToStorage()
-    var message = new(protocol)
-    message.Type = TYPE_PUT
-    var err error
+	var err error
+	var projectName string
+	var message string
 	for {
 		m := h.Get()
 		if m == nil {
@@ -90,34 +86,19 @@ func (h *Handler) ringBufferLoop() {
 		if h.DrainURI != "" {
 			drain.SendToDrain(m.String(), h.DrainURI)
 		}
-        message.Payload = m.String()
-        message.ProjectName, err = getProjectName(message.Payload); if err != nil {
-            message.Payload = fmt.Sprintln(err)
-        }
-        inChannel <- message
+		message = m.String()
+		projectName, err = getProjectName(message)
+		if err != nil {
+			projectName = "unknown"
+			message = fmt.Sprintln(err)
+		}
+		addToStorage(projectName, message)
 	}
 	h.End()
 }
 
-// Actually only this function which should work in goroutines have access to log storage
-func accessToStorage() {
-    var message = new(protocol)
-    for {
-        message = <- inChannel
-        switch message.Type {
-            default: continue
-            case TYPE_PUT:
-                addToStorage(message.ProjectName, message.Payload)
-            case TYPE_REQUEST:
-                message.Payload = getFromStorage(message.ProjectName, message.LinesNumber)
-                outChannel <- message
-        }
-    }
-}
-
 // Only one http handler which process requests
 func httpHandler(w http.ResponseWriter, r *http.Request) {
-    var message = new(protocol)
 	if r.Method == "POST" {
 		match := regexpForPost.FindStringSubmatch(r.RequestURI)
 
@@ -132,10 +113,7 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Fprintln(w, "Could not read from post request, no 'message' param in POST")
 			return
 		}
-        message.Type = TYPE_PUT
-        message.ProjectName = match[1]
-        message.Payload = value[0]
-        inChannel <- message
+		addToStorage(match[1], value[0])
 		return
 	}
 
@@ -146,21 +124,18 @@ func httpHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-    logLines, err := strconv.Atoi(match[2])
+	logLines, err := strconv.Atoi(match[2])
 	if err != nil {
 		fmt.Fprintln(w, "Unable to get log lines parameter from request")
 		return
 	}
-    message.Type = TYPE_REQUEST
-    message.ProjectName = match[1]
-    message.LinesNumber = logLines
-    inChannel <- message
-    message = <- outChannel
+
+	getFromStorage(match[1], logLines)
 
 	if err != nil {
 		fmt.Fprintln(w, err)
 	} else {
-        fmt.Fprint(w, message.Payload)
+		fmt.Fprint(w, match[1])
 	}
 }
 
